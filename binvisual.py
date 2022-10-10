6dc6ad23675f2f5c91489352c5d535c7a7c1db9f
Rahul J <jrahul1112@gmail.com>
2022-10-10 11:11:40 +0530

    Third Commit

--- binvisual.py
+++ binvisual.py
@@ -1,3 +1,5 @@
+#  source /d/Documents/SEM 7/Binary Visualizer/venv/Scripts/activate
+
 import base64
 import csv
 import hashlib
@@ -7,7 +9,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 import os
-
+from urllib.request import urlopen
 
 
 class BinaryVisualizer():
@@ -26,16 +28,16 @@ class BinaryVisualizer():
                 #     break
                 try:
                     if not os.path.exists(f'binaryFiles\\binaryFiles{index}.bin'):
-                        if index > 1047:
+                        if index > 26083:
                             self.createBinaryFiles(index, row)
-                    print(str(index) + ' passed')
                 except Exception as e:
                     print(index, e)
         
     def createBinaryFiles(self, index, row):
         url = 'http://' + row[0]
-        sourceCode = urllib.request.urlopen(url)
-        binaryCode = str(base64.b64encode(sourceCode.read()))[2:-1]
+        sourceCode = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        # sourceCode = urllib.request.urlopen(url)
+        binaryCode = str(base64.b64encode(urlopen(sourceCode).read()))[2:-1]
         binFilePath = f'binaryFiles\\binaryFiles{index}.bin'
         binFile = open(binFilePath, 'w')
         binFile.write(binaryCode)
@@ -80,6 +82,7 @@ class BinaryVisualizer():
         # img = Image.open(output_filename)
         # img = img.crop((left, top, right, bottom))
         # img.save(output_filename)
+        print(str(index) + ' passed')
 
 
 BinaryVisualizer()
